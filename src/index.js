b1dc8ff1b7fb5038ec6f1fd15f1125dc485f8294
Pumpking <gn131131@sina.com>
2020-03-03 14:25:14 +0800

    npm创建，添加1.7.9版本angularJS

---
+++ src/index.js
@@ -0,0 +1,54 @@
+/*
+ * @Description: 
+ * @Version: 1.0
+ * @Autor: Pumpking
+ * @Date: 2020-02-11 16:13:25
+ * @LastEditors: Pumpking
+ * @LastEditTime: 2020-03-03 14:05:16
+ */
+"use strict";
+
+import "./vendor/flexble/base.css";
+import "./vendor/flexble/flexible";
+
+import "./style.css";
+
+import $ from "jquery";
+
+$(document).ready(() => {
+  compatibility();
+  init();
+  hotModuleSet();
+});
+
+function init() {
+  
+}
+// ts 兼容性写法不可行
+function compatibility() {
+  if (!window.requestAnimationFrame) {
+    window.requestAnimationFrame = (
+      window.webkitRequestAnimationFrame ||
+      window.mozRequestAnimationFrame ||
+      window.msRquestAniamtionFrame ||
+      window.oRequestAnimationFrame ||
+      function (callback) {
+        return setTimeout(callback, Math.floor(1000 / 60))
+      }
+    )
+  }
+}
+
+function hotModuleSet() {
+  if (module.hot) {
+    module.hot.accept("./js/main.ts", () => {
+      console.log("热部署模块更新");
+      init();
+    });
+  }
+
+
+  if (process.env.NODE_ENV !== "production") {
+    console.log("Looks like we are in development mode!");
+  }
+}
