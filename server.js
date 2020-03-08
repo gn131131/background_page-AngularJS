91ff5db5e89a18a3cf36354e3a10a25706e63e74
Pumpking <gn131131@sina.com>
2020-03-08 17:27:27 +0800

    修改server.js，添加调试json，修改gitignore

--- server.js
+++ server.js
@@ -4,13 +4,45 @@
  * @Autor: Pumpking
  * @Date: 2020-03-07 21:50:07
  * @LastEditors: Pumpking
- * @LastEditTime: 2020-03-07 21:55:17
+ * @LastEditTime: 2020-03-08 17:26:41
  */
-var http = require("http");
+'use strict';
 
-http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World\n');
-}).listen(8201);
+var
+    fs = require('fs'),
+    url = require('url'),
+    path = require('path'),
+    http = require('http');
 
-console.log('Server running at http://127.0.0.1:8201/');
+// 从命令行参数获取root目录，默认是当前目录:
+var root = path.resolve(process.argv[2] || '.');
+
+console.log('Static root dir: ' + root);
+
+// 创建服务器:
+var server = http.createServer(function (request, response) {
+  
+    var pathname = url.parse(request.url).pathname;
+
+    var filepath = path.join(root, pathname);
+    // 获取文件状态:
+    fs.stat(filepath, function (err, stats) {
+      console.log(filepath, err, stats.isFile())
+        if (!err && stats.isFile()) {
+            // 没有出错并且文件存在:
+            console.log('200 ' + request.url);
+            // 发送200响应:
+            response.writeHead(200);
+            // 将文件流导向response:
+            fs.createReadStream(filepath).pipe(response);
+        } else {
+            // 出错了或者文件不存在:
+            console.log('404 ' + request.url);
+            // 发送404响应:
+            response.writeHead(404);
+            response.end('404 Not Found');
+        }
+    });
+});
+
+server.listen(8080);
