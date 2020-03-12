1ba0ba2659db0d92934c528b838a4cee2f59f960
Pumpking <gn131131@sina.com>
2020-03-12 15:37:54 +0800

    dist update

--- dist/8.build.js
+++ dist/8.build.js
@@ -1,4 +1,4 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{13:function(module,exports,__webpack_require__){
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{26:function(module,exports,__webpack_require__){
 /**
  * oclazyload - Load modules on demand (lazy load) with angularJS
  * @version v1.0.10
