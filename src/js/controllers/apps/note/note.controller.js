ff3c7eff708a1d57f5a0b3fae09df6e00a05a0c9
Pumpking <gn131131@sina.com>
2020-03-16 14:29:57 +0800

    应用-笔记、联系人页面移植完成

---
+++ src/js/controllers/apps/note/note.controller.js
@@ -0,0 +1,48 @@
+/*
+ * @Description: 
+ * @Version: 1.0
+ * @Autor: Pumpking
+ * @Date: 2020-03-16 14:13:05
+ * @LastEditors: Pumpking
+ * @LastEditTime: 2020-03-16 14:24:43
+ */
+import {
+  note
+} from "../../../constants/database";
+
+export default angular.module('apps.note.controller', [])
+  .controller('NoteCtrl', ['$scope', '$http', function ($scope, $http) {
+    $scope.notes = note.notes;
+    // set default note
+    $scope.note = $scope.notes[0];
+    $scope.notes[0].selected = true;
+
+    $scope.colors = ['primary', 'info', 'success', 'warning', 'danger', 'dark'];
+
+    $scope.createNote = function () {
+      var note = {
+        content: 'New note',
+        color: $scope.colors[Math.floor((Math.random() * 3))],
+        date: Date.now()
+      };
+      $scope.notes.push(note);
+      $scope.selectNote(note);
+    }
+
+    $scope.deleteNote = function (note) {
+      $scope.notes.splice($scope.notes.indexOf(note), 1);
+      if (note.selected) {
+        $scope.note = $scope.notes[0];
+        $scope.notes.length && ($scope.notes[0].selected = true);
+      }
+    }
+
+    $scope.selectNote = function (note) {
+      angular.forEach($scope.notes, function (note) {
+        note.selected = false;
+      });
+      $scope.note = note;
+      $scope.note.selected = true;
+    }
+
+  }]);
--- src/js/router.js
+++ src/js/router.js
@@ -4,7 +4,7 @@
  * @Autor: Pumpking
  * @Date: 2020-03-03 18:23:06
  * @LastEditors: Pumpking
- * @LastEditTime: 2020-03-13 15:43:15
+ * @LastEditTime: 2020-03-16 14:26:33
  */
 const ocLazyLoadFn = ($ocLazyLoad, urls) => {
   let arr = [];
@@ -51,6 +51,29 @@ const router = ['$urlRouterProvider', '$stateProvider', ($urlRouterProvider, $st
       resolve: {
         loadLoginController: ($ocLazyLoad) => {
           return ocLazyLoadFn($ocLazyLoad, ['./controllers/app/dashboard/dashboard']);
+        }
+      }
+    })
+    .state('apps', {
+      abstract: true,
+      url: '/apps',
+      template: require('./controllers/apps/layout.html').default
+    })
+    .state('apps.note', {
+      url: '/note',
+      template: require('./controllers/apps/note/note.template.html').default,
+      resolve: {
+        loadNoteController: ($ocLazyLoad) => {
+          return ocLazyLoadFn($ocLazyLoad, ['./controllers/apps/note/note']);
+        }
+      }
+    })
+    .state('apps.contact', {
+      url: '/contact',
+      template: require('./controllers/apps/contact/contact.template.html').default,
+      resolve: {
+        loadContactController: ($ocLazyLoad) => {
+          return ocLazyLoadFn($ocLazyLoad, ['./controllers/apps/contact/contact']);
         }
       }
     })
